534fde139aa39af42d61eff296f593636d129b42
Flipp3rrr <flipp3rrr@protonmail.com>
2021-02-03 21:23:35 +0100

    Consistency, code comments, exclude tkinter

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 import py2exe, sys, os
 
-sys.argv.append('py2exe')
+sys.argv.append("py2exe")
 
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows = [{'script': "BSOD.py"}],
+    # Use "bundle_file": 1 to create a single EXE including everything
+    # Use "compressed": True for a smaller file size
+    options = {"py2exe": {"bundle_files": 1, "compressed": True}},
+    windows = [{"script": "BSOD.py"}],
     zipfile = None,
+    # Exclude tkinter because it's not needed
+    excludes = ["Tkconstants","Tkinter","tcl"]
 )
