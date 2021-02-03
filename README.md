7cdc34130ef1e183ff4d717f676ff186fade2f5f
Flipp3rrr <flipp3rrr@protonmail.com>
2021-02-03 14:19:59 +0100

    Create single EXE

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # WinBSOD
-A "harmless" EXE file to get a BSOD, this program needs Firefox to function.
+A "harmless" EXE file to get a BSOD.
 ## Dependencies
 * py2exe
+* Firefox (on the target machine)
 ## Building
 Run `python setup.py` to create the EXE
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['BSOD.py'])
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{'script': "BSOD.py"}],
+    zipfile = None,
+)
