1a0e117cb0154727ae60c27ab02e560dbd9864b4
Flipp3rrr <flipp3rrr@protonmail.com>
2021-02-03 21:25:13 +0100

    Detect wether running from an EXE or PY file, and print output accordingly

--- BSOD.py
+++ BSOD.py
@@ -1,18 +1,31 @@
-import time
-import os
+# Temporary fix to suppress the DeprecationWarning for the `imp` module
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Wait for 10 seconds
-print("BSOD in 10 seconds", end="\r")
-time.sleep(.5)
-print("BSOD in 10 seconds.", end="\r")
-time.sleep(.5)
-print("BSOD in 10 seconds..", end="\r")
-time.sleep(.5)
-print("BSOD in 10 seconds...")
-for n in range(0, 11):
-  print("Progress >> %s/10" % n, end="\r")
-  time.sleep(1)
-print("Payload activated!")
+import time, os, sys, imp
+
+sys.argv.append("-W ignore::DeprecationWarning")
+
+# Detect wether you're running an EXE or PY file
+def isEXE():
+  return (hasattr(sys, "frozen") or # new py2exe
+          hasattr(sys, "importers") # old py2exe
+          or imp.is_frozen("__main__")) # tools/freeze
+
+if isEXE() == False:
+  print("EXE: %s" % isEXE())
+  # Wait for 10 seconds
+  print("BSOD in 10 seconds", end="\r")
+  time.sleep(.5)
+  print("BSOD in 10 seconds.", end="\r")
+  time.sleep(.5)
+  print("BSOD in 10 seconds..", end="\r")
+  time.sleep(.5)
+  print("BSOD in 10 seconds...")
+  for n in range(0, 11):
+    print("Progress >> %s/10" % n, end="\r")
+    time.sleep(1)
+  print("Payload activated!")
 
 # Windows BSODs when you enter `\\.\GLOBALROOT\Device\ConDrv\KernelConnect`
 # into a browser. Sadly this doesn't work on the built-in Internet Explorer,
